b37cd1051441813ebd2ad44c5bd67f4782b6d5cb
indrajit <idas@zaloni.com>
2017-07-20 19:08:54 +0530

    added some inline comments

--- spark-test/src/main/java/com/test/spark/session/SparkSessionProvider.java
+++ spark-test/src/main/java/com/test/spark/session/SparkSessionProvider.java
@@ -2,15 +2,20 @@ package com.test.spark.session;
 
 import org.apache.spark.sql.SparkSession;
 
+/**
+ * The entry point into all functionality in Spark is the SparkSession class. To create a basic SparkSession, just use
+ * SparkSession.builder()
+ *
+ */
 public class SparkSessionProvider {
-     
+
      private static SparkSession sparkSession;
-     
-     static{
+
+     static {
           sparkSession = SparkSession.builder().appName("My_App").master("local").getOrCreate();
      }
-     
-     public static SparkSession getSparkSession(){
+
+     public static SparkSession getSparkSession() {
           return sparkSession;
      }
 
